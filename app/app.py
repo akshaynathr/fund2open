72563c81b57ca17b31863fc0f5704d75400433f4
Akshaynath R <akshaynathr@gmail.com>
2018-07-10 09:27:18 +0530

    render template changed

--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
-from flask import Flask 
+from flask import Flask ,render_template
+
 
 app = Flask(__name__)
 
 @app.route('/home')
 @app.route('/')
 def home():
-    return "Welcome to fund to open"
+    return render_template('homePage.html')
